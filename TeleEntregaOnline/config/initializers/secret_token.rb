3409e5f5b19fc406f7b4366061bb969bb060af92
thiagocorrea <thcinter@yahoo.com.br>
2012-11-02 01:45:30 -0200

    Check-in dos Arquivos

---
+++ TeleEntregaOnline/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TeleEntregaOnline::Application.config.secret_token = 'cb3cca6bf141c59cd279eba31142657ea6d4f4c3294f491a5d1fb7003322d5fdccbe39de9479c27c3d5668ef52524b4d6f8f13a8ad9b15f6152fbbabe36136c4'
