33426f4980d05b9ac131a32f13626953193b4e91
v-marcia <vargas.marcia@gmail.com>
2012-12-06 20:26:24 -0200

    Layout de Aplicacao

--- TeleEntregaOnline/config/routes.rb
+++ TeleEntregaOnline/config/routes.rb
@@ -1,4 +1,6 @@
 TeleEntregaOnline::Application.routes.draw do
+  get "login/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ TeleEntregaOnline::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'login#index'
 
   # See how all your routes lay out with "rake routes"
 
